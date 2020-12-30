644089cc2c16e9a596309ec90b0752eec076f310
mikoselene <miko.selene@gmail.com>
2020-12-30 13:42:49 +0000

    variaveis

---
+++ main.js
@@ -0,0 +1,29 @@
+//variáveis
+var primeiroNome = 'Leonor'; // string
+var segundoNome = 'Selene'; // string
+var idade = 14; // number
+var mulher = true; // boolean
+
+console.log(primeiroNome, segundoNome);
+
+// arrays
+var pessoaArray = ['Leonor', 'Selene', 14];
+console.log(pessoaArray[0], pessoaArray[1]);
+
+//objectos
+var pessoaObjecto = {
+  primeiro: 'Leonor',
+  segundo: 'Selene',
+  idade: 14,
+  mulher: true,
+};
+
+console.log(
+  pessoaObjecto.primeiro,
+  pessoaObjecto.segundo
+);
+
+//operadores aritmeticos
+var anoatual = 2020;
+var anonascimento = 2006;
+console.log(anoatual - anonascimento);
