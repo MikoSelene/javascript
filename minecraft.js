29f36a04001706318e467d0effce04b28bacb291
cdeath <github@nunopereira.net>
2021-01-06 19:06:00 +0000

    minecraft materials

---
+++ minecraft.js
@@ -0,0 +1,16 @@
+var materials = [
+  {
+    name: 'diamond',
+    color: 'cyan',
+    rare: true,
+    locations: ['underground', 'caverns'],
+    uses: ['crafting'],
+  },
+  {
+    name: 'coal',
+    color: 'black',
+    rare: true,
+    locations: ['everywhere'],
+    uses: ['crafting', 'fuel'],
+  },
+];
