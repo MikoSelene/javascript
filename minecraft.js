0129b4f491fbd2a66e21184bb39d4c27ea6759ea
mikoselene <miko.selene@gmail.com>
2021-01-06 20:14:38 +0000

    iterar sobre os materiais

--- minecraft.js
+++ minecraft.js
@@ -1,3 +1,5 @@
+var xp = 0;
+
 var materials = [
   {
     name: 'diamond',
@@ -5,12 +7,34 @@ var materials = [
     rare: true,
     locations: ['underground', 'caverns'],
     uses: ['crafting'],
+    xp:5000,
   },
   {
     name: 'coal',
     color: 'black',
-    rare: true,
+    rare: false,
     locations: ['everywhere'],
     uses: ['crafting', 'fuel'],
+    xp:200
   },
+  {
+    name: 'emerald',
+    color: 'green',
+    rare: true,
+    locations: ['underground', 'caverns','mountains'],
+    uses: ['trade'],
+    xp:7500
+  }
 ];
+
+console.log(materials);
+
+//for(var i = 0; i <materials.length;i++){
+//console.log(materials[i])
+//}
+
+materials.forEach(function(material) {
+  xp += material.xp;
+  console.log(xp);
+
+});
