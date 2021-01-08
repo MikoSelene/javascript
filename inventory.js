2172aa0d17f39108441a077afd9d01f22acadd1a
cdeath <github@nunopereira.net>
2021-01-08 13:22:53 +0000

    changes

---
+++ inventory.js
@@ -0,0 +1,18 @@
+/*
+* TRABALHO DE CASA
+*/
+
+/*
+* criar um array chamado inventory
+* que tenha objectos lá dentro com as suas propriedades
+* name, quantity, e outras que queiras, decide tu
+* a ideia é recriar o saco, inventário, etc...
+*/
+
+/*
+* imprimir para a consola todos os items
+*/
+
+/*
+* imprimir na console a soma das quantidades todas de todos os items
+*/
