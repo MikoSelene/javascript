078d7f00ba6b607774401d3971721645a0a63335
cdeath <github@nunopereira.net>
2021-01-11 21:36:43 +0000

    enunciado

--- inventory.js
+++ inventory.js
@@ -1,3 +1,22 @@
+/*
+* TRABALHO DE CASA
+*/
+
+/*
+* criar um array chamado inventory
+* que tenha objectos lá dentro com as suas propriedades
+* name, quantity, e outras que queiras, decide tu
+* a ideia é recriar o saco, inventário, etc...
+*/
+
+/*
+* imprimir na consola todos os items
+*/
+
+/*
+* imprimir na consola a soma das quantidades todas de todos os items
+*/
+
 var materials = [
     {
       name: 'diamond',
@@ -27,21 +46,3 @@ var materials = [
       xp:550
     }
   ];
-/*
-* TRABALHO DE CASA
-*/
-
-/*
-* criar um array chamado inventory
-* que tenha objectos lá dentro com as suas propriedades
-* name, quantity, e outras que queiras, decide tu
-* a ideia é recriar o saco, inventário, etc...
-*/
-
-/*
-* imprimir na consola todos os items
-*/
-
-/*
-* imprimir na consola a soma das quantidades todas de todos os items
-*/
